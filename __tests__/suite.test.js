e7d40a76acf06aeab7c487a3f31aee7a97e5b3b2
Carlos Reyes <carlos.reyes@theksquaregroup.com>
2022-08-31 09:41:14 -0500

    feat: added test cases and instructions

--- __tests__/suite.test.js
+++ __tests__/suite.test.js
@@ -1,6 +1,10 @@
-const { func } = require('../exercise');
+const { palindrome } = require('../exercise');
 
-test('Testing the function exercise', () => { 
-    expect(func()).toBeDefined();
-    
+test('Testing the palindrome exercise', () => { 
+    expect(palindrome('a')).toBeDefined();
+    expect(palindrome('race a car')).toBe(false)
+    expect(palindrome('madam')).toBe(true);
+    expect(palindrome('taco cat')).toBe(true);
+    expect(palindrome('noon')).toBe(true);
+    expect(palindrome('almost a palindrome')).toBe(false);
  })
--- exercise.js
+++ exercise.js
@@ -1,5 +1,20 @@
+/* * Instructions *
+    A palindrome is a word or phrase that's read or spelled the same way backwards or forwards when we ignore spaces,
+    punctuation or symbols.
+
+    Note that for this exercise you will only need to remove spaces as no symbols or punctuations will be present in the
+    test cases.
+
+    You will receive a string as argument str and you need to return TRUE if the string is a palindrome or false if the string
+    is not a palindrome
+
+    Hint: You can use the String method split() to obtain an array with all the characters from the string.
+
+
+*/
+
 /** DO NOT CHANGE THE FUNCTION NAME **/
-const func = (c) => {
+const palindrome = (str) => {
     /* Only make changes below this comment */
     
     
@@ -11,4 +26,4 @@ const func = (c) => {
 
 
 /** DO NOT CHANGE THE LINE BELOW **/
-module.exports.func = func;
+module.exports.palindrome = palindrome;
