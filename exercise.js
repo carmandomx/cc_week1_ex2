76414d2e9f58fa427465fbc5216e276bd4c28c9f
Axcel Espinal <axcel.espinal@itksquare.edu.mx>
2022-09-19 16:22:49 -0400

    Added the code to the function

--- exercise.js
+++ exercise.js
@@ -16,14 +16,22 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const palindrome = (str) => {
     /* Only make changes below this comment */
-    
-    
+    str = str.replace(/\s/g, ''); //remove spaces from string
+    var str = str.split(""); //getting an array with all the characters from the string;
+    var rev = [...str].reverse(); //creating a temporal array which reverses the original one
+
+    for(let i=0, j=0; i<str.length; i++, j++) { //create a for loop to iterate the arrays
+        if (str[i] == rev[j]) { //if the first element of the original array is the same character as the reversed one, continue
+            continue;
+        }
+        else { //if the first elements are not the same, it means it's not a palindrome, so we return false
+            return false;
+        }
+    } 
+    return true; //as the loop breaks when it finds a mismatch, if it doesn't find it it's because the string is a palindrome, so we return true
+
     /* Only make changes below this comment */
 }
-
-
-
-
 
 /** DO NOT CHANGE THE LINE BELOW **/
 module.exports.palindrome = palindrome;
