3148168446060307a8abef224f98bc490f1e97dd
ArgenisGonzalez-Ksquare <Argenis.Gonzalez@itksquare.edu.mx>
2022-09-20 11:09:16 -0400

    Add my solution for exercise2

--- exercise.js
+++ exercise.js
@@ -9,15 +9,27 @@
     is not a palindrome
 
     Hint: You can use the String method split() to obtain an array with all the characters from the string.
-
-
 */
 
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const palindrome = (str) => {
     /* Only make changes below this comment */
     
-    
+    let reverseStr = ""; //We create a empty variable to storage the reverse string
+    str = str.replace(/ /g, ''); //We use the function replace to replace all de blank space of our String. 
+
+    //With the str without blank space we use a "for" starting from the last to firts character to storage the reverseString
+    for (var i = str.length -1; i >= 0; i--) {
+        reverseStr += str[i];
+    }
+
+    //we do the comparison between the string and the reverseString
+    if(str != reverseStr) {
+        return false;    //If not are equal the return false
+    }
+    else {
+        return true;   //#Else return true
+    }
     /* Only make changes below this comment */
 }
 
