a81543149741f1e4ca01fd19cbaf9ff69d37a8b1
Ivanna Nouel <Ivanna.Nouel@itksquare.edu.mx>
2022-09-19 21:48:21 -0500

    Done

--- exercise.js
+++ exercise.js
@@ -19,6 +19,41 @@ const palindrome = (str) => {
     
     
     /* Only make changes below this comment */
+
+    //regex = regular expression 
+    //\w removes all non-alphanumeric characters(“\W” matches the non-word characters.)
+    //The g means Global, and causes the replace call to replace all matches, not just the first one.
+    /*
+    [  ] are the start and end of a character set.
+    _ is the "_" character.
+    / mark the beginning and end of a regular expression. */
+    var re = /[\W_]/g;
+
+
+    //The toLowerCase method returns the value of the string converted to lowercase.
+    //The replace() method returns a new string with one, some, or all matches of a pattern replaced by a replacement .
+    
+    
+    //we convert capital leter words to lowercase words using .toLowerCase()
+    //we replace the spaces with replace and use 're' (that is used to detect non-word characters ) and replace them with not space
+    var low = str.toLowerCase().replace(re,"");
+
+    //Since a palindrome is a word or phrase that's read or spelled the same way backwards or forwards when we ignore spaces, punctuation or symbols. 
+    //That's why we use this line so we can separate the words into letters, to create an a array with the letters, then with reverse() reverse it and then join it, to see if the word spelled the same way backwards or forwards when we ignore spaces.
+
+    //The split() method splits a string into an array of substrings. The split() method returns the new array. The split() method does not change the original string.
+    //Reverses an array in place and returns the reference to the same array, the first array element now becoming the last, and the last array element becoming the first.
+    //The join() method joins all the elements of an array (or an array-like object) into a string and returns this string
+    var check = low.split("").reverse().join(""); 
+
+
+    //if the word converted and replaced is equals to the array of words that where reversed then the palindrome is happenning
+    if (low == check){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
 
