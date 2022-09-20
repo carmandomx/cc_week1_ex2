0a9353744f24563497ec42718e3819f3c83af9a7
Brandon Hernandez <Brandon.Hernandez@itksquare.edu.mx>
2022-09-20 16:08:17 -0500

    Final Solution W/comments

--- exercise.js
+++ exercise.js
@@ -16,11 +16,14 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const palindrome = (str) => {
     /* Only make changes below this comment */
-    let
-    
+    let newString = str.replace(/\s/g, "").toLowerCase(); // researched a way to remove spaces and found this regular expression, this will remove de space in the global string and replace it with "" which is an empty space, and creates a new string in lower case with no spaces
+    let reversedString = newString.split("").reverse().join(""); // I created a new string that will be spelled backwards, I split it per characters that will create an array, then it reverses it and joins it back for the comparison, i used the split method to check if everything is working properly meaning that it's removing the spaces, if there were an error the array whould show an empty string.
+    if (newString === reversedString){ //the comparison between the altered string without spaces and in lower case, and the other string which should be reversed, without spaces and in lower case as well
+        return true; // if the two strings match then the original sentence is a palindrome
+    }
+    return false; // if not then it will return false
     /* Only make changes below this comment */
 }
-
 
 
 
