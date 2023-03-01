2bbb50b0dd9798c5c3a392bd78631031a3b26144
Alexis Guevara <alexis.guevara@itksquare.edu.mx>
2023-02-28 16:40:33 -0800

    Ex2 - test passed

--- exercise.js
+++ exercise.js
@@ -16,8 +16,24 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const palindrome = (str) => {
     /* Only make changes below this comment */
+
+    /* Expresion regular para remover espacios en blanco */
+    const regExp = /\s+/g;
+
+    /* Se le asigna a una variable el valor de 'str' pero convirtiendo todas las letras a minusculas y removiendo los espacios en blanco */
+    let lowRegExp = str.toLowerCase().replace(regExp, '');
+
+    /* Se divide la cadena letra por letra con método split, con el método reverse voteamos la cadena y con el método join volvemos a integrar la cadena */
+    let newStr = lowRegExp.split('').reverse().join('');
     
-    
+    /* Comparar la cadena 'normal' vs la cadena en reversa */
+    if(newStr === lowRegExp) {
+        return true;
+    }
+    else {
+        return false;
+    }
+        
     /* Only make changes below this comment */
 }
 
