f8bc95d55e8d2e13a11698e9a6bc214583bfe370
Alan Villanueva <alan.villanueva@itksquare.edu.mx>
2023-02-28 22:34:12 -0600

    solved

--- exercise.js
+++ exercise.js
@@ -15,10 +15,32 @@
 
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const palindrome = (str) => {
-    /* Only make changes below this comment */
+    let array=(str).split(' ') /* creating array consisting only of words (spaces retired)  ------ > anita lava la tina*/
+    let suma = ''
     
-    
-    /* Only make changes below this comment */
+    for (let i = 0; i<array.length; i++){ /* adjoint all words to form only one ------> anitalavalatina */
+        suma=suma+array[i]
+    }
+
+    let len = suma.length /* quantity of letters*/
+
+    if (len%2===0){ /* Checking for the parity of the quantity of letters. Even case*/
+        for (let i = 0; i < (len)/2;i++){
+            if (suma[i]!==suma[len-1-i]){ /* Check equality on 'contrary' letters in the word. Meaning the first with the last, the second one with the penultimate... */
+                
+                return false
+            }
+        }
+    return true
+    } else { /* odd case*/
+        for (let i = 0; i < Math.trunc((len)/2)+1;i++){
+            if (suma[i]!==suma[len-1-i]){
+                
+                return false
+            }
+        }
+        return true
+    }
 }
 
 
